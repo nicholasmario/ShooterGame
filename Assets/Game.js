d2ebffb6b3b1c73ca5636551505b9f641e375fc2
mario.wardhana@ntu.edu.sg <mario.wardhana@ntu.edu.sg>
2014-09-10 21:09:47 +0800

    Score and money calculations

--- Assets/Game.js
+++ Assets/Game.js
@@ -3,11 +3,21 @@
 private var remainingEnemy: int;
 private var score: int = 0;
 private var level: int = 0;
+private var currentEnemy: int;
+public var increaseRate: int = 1;
+public var initSpawnRate: float = 3.0f;
+public var spawnRateIncRate: float = 0.5f;
 
 function EnemyHit() {
 	remainingEnemy--;
 	score += 100 * level;
 	if (remainingEnemy <= 0) {
+		level++;
+		currentEnemy = currentEnemy + increaseRate;
+		remainingEnemy = currentEnemy;
 
+		if (initSpawnRate > 0.5) {
+			initSpawnRate -= spawnRateIncRate;
+		}
  	}
 }
