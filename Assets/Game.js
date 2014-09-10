b2781ea4921818135d3cc31362fc7eda26cdeee0
mario.wardhana@ntu.edu.sg <mario.wardhana@ntu.edu.sg>
2014-09-10 21:11:11 +0800

    Speed

--- Assets/Game.js
+++ Assets/Game.js
@@ -3,11 +3,21 @@
 private var remainingEnemy: int;
 private var score: int = 0;
 private var level: int = 0;
+public var initMoveSpeed: float = 0.5f;
+public var moveSpeedIncRate: float = 0.5f;
+public var initShootRate: float = 3.0f;
+public var shootRateIncRate: float = 0.5f;
 
 function EnemyHit() {
 	remainingEnemy--;
 	score += 100 * level;
 	if (remainingEnemy <= 0) {
-
+		if (initMoveSpeed < 8) {
+			initMoveSpeed += moveSpeedIncRate;
+		}		
+		if (initShootRate > 0.5) {
+			initShootRate -= shootRateIncRate;
+		}
  	}
 }
+
