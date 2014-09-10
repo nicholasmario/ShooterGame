827dd39fff203ed50d9bd1b919b19a1c2551b8e8
mario.wardhana@ntu.edu.sg <mario.wardhana@ntu.edu.sg>
2014-09-10 21:00:22 +0800

    Initial commit

---
+++ Assets/Game.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+private var remainingEnemy: int;
+private var score: int = 0;
+private var level: int = 0;
+
+function EnemyHit() {
+	remainingEnemy--;
+	score += 100 * level;
+	if (remainingEnemy <= 0) {
+
+ 	}
+}
